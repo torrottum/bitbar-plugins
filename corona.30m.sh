f4424b05833a5fda1bf856dd1e37996dfb2b6540
Tor Røttum <tor@rottum.me>
2020-03-16 11:52:50 +0100

    add alternative operator for null values

--- corona.30m.sh
+++ corona.30m.sh
@@ -8,19 +8,19 @@ counties=$(curl -s "https://redutv-api.vg.no/corona/v1/sheets/norway-table-overv
 municipalities=$(curl -s "https://redutv-api.vg.no/corona/v1/sheets/norway-table-overview/?region=municipality")
 hospitalized=$(curl -s "https://redutv-api.vg.no/corona/v1/hospitalized")
 
-echo "🦠 $(echo "$counties" | jq -r '.totals.confirmed')"
+echo "🦠 $(echo "$counties" | jq -r '.totals.confirmed // "-"')"
 echo "---"
-echo "Confimed: $(echo "$counties" | jq -r '.totals.confirmed')"
-echo "Dead: $(echo "$counties" | jq -r '.totals.dead')"
-echo "Recovered: $(echo "$counties" | jq -r '.totals.recovered')"
+echo "Confimed: $(echo "$counties" | jq -r '.totals.confirmed // "-"')"
+echo "Dead: $(echo "$counties" | jq -r '.totals.dead // "-"')"
+echo "Recovered: $(echo "$counties" | jq -r '.totals.recovered // "-"')"
 echo "---"
-echo "Hospitalized: $(echo "$hospitalized" | jq -r '.current.total.hospitalized')"
-echo "Respiratory: $(echo "$hospitalized" | jq -r '.current.total.respiratory')"
-echo "Infected Employees: $(echo "$hospitalized" | jq -r '.current.total.infectedEmployees')"
-echo "Quarantined Employees: $(echo "$hospitalized" | jq -r '.current.total.quarantineEmployees')"
+echo "Hospitalized: $(echo "$hospitalized" | jq -r '.current.total.hospitalized // "-"')"
+echo "Respiratory: $(echo "$hospitalized" | jq -r '.current.total.respiratory // "-"')"
+echo "Infected Employees: $(echo "$hospitalized" | jq -r '.current.total.infectedEmployees // "-"')"
+echo "Quarantined Employees: $(echo "$hospitalized" | jq -r '.current.total.quarantineEmployees // "-"')"
 echo "---"
 echo "Infected per county"
-echo "$counties" | jq -r '.cases[] | [.name, .confirmed, .dead, .recovered] | @tsv' |
+echo "$counties" | jq -r '.cases[] | [.name, .confirmed // "-", .dead // "-", .recovered // "-"] | @tsv' |
     while IFS=$'\t' read -r name confirmed dead recovered; do
         echo "$name: $confirmed"
         echo "----"
@@ -30,7 +30,7 @@ echo "$counties" | jq -r '.cases[] | [.name, .confirmed, .dead, .recovered] | @t
     done
 echo "---"
 echo "Hospitalized"
-echo "$hospitalized" | jq -r '.current.hospitals[] | [.name, .hospitalized, .respiratory, .infectedEmployees, .quarantineEmployees] | @tsv' |
+echo "$hospitalized" | jq -r '.current.hospitals[] | [.name, .hospitalized // "-", .respiratory // "-", .infectedEmployees // "-", .quarantineEmployees // "-"] | @tsv' |
     while IFS=$'\t' read -r name hospitalized respiratory infectedEmployees quarantineEmployees; do
         echo "$name: $hospitalized"
         echo "----"
